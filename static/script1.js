6291fcbe9a41533cbe93f0b6888de43801fadeeb
hnoel <8425141+hnoel@users.noreply.github.com>
2026-01-14 21:39:31 +0100

    Create script1.js

---
+++ static/script1.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  FlexDashboard.init({
+    theme: "cosmo",
+    fillPage: false,
+    orientation: "rows",
+    storyboard: false,
+    defaultFigWidth: 576,
+    defaultFigHeight: 460,
+    defaultFigWidthMobile: 360,
+    defaultFigHeightMobile: 460,
+    resize_reload: true
+  });
+});
